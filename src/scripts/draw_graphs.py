4431ae339872e14cd61934230a3eb998e303c704
maksymkravchenko <maksym.kravchenko@tu-dortmund.de>
2025-05-02 22:29:16 +0200

    Added photos of medium datasets

--- src/scripts/draw_graphs.py
+++ src/scripts/draw_graphs.py
@@ -1,11 +1,11 @@
-from src.helpers.config import CURRENT_DATASETS
+from src.helpers.config import CURRENT_DATASETS, MEDIUM_DATASETS
 from src.helpers.loaders.mat_loader import load_graph_from_mat
 from src.helpers.plotters.nx_graph_plotter import to_networkx_graph
 from src.structure.data_set import DataSetSize
 
 def main():
-    for dataset in CURRENT_DATASETS:
-        labels, graph = load_graph_from_mat(name=dataset, size=DataSetSize.SMALL)
+    for dataset in MEDIUM_DATASETS:
+        labels, graph = load_graph_from_mat(name=dataset, size=DataSetSize.MEDIUM)
         to_networkx_graph(graph=graph, visualize=True, title=dataset)
 
 if __name__ == "__main__":
