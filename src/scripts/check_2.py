9027b6356135a35726e4e7a9a797c77589232d6d
masss <maksimkravchenkoukraine@gmail.com>
2025-05-16 23:55:42 +0200

    Experimenting with GitHub Actions 20

---
+++ src/scripts/check_2.py
@@ -0,0 +1,11 @@
+import train_structure_and_feature_m
+import train_structure_and_feature_2_m
+import train_structure_and_feature_3_m
+
+def main():
+    train_structure_and_feature_m.main()
+    train_structure_and_feature_2_m.main()
+    train_structure_and_feature_3_m.main()
+
+if __name__ == "__main__":
+    main()
