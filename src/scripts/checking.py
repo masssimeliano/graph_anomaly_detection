5d8264f5bffae20b2dcb58635cf7553a37cf1b57
masss <maksimkravchenkoukraine@gmail.com>
2025-05-17 08:19:53 +0200

    Experimenting with GitHub Actions 22

--- src/scripts/checking.py
+++
@@ -1,9 +0,0 @@
-import train_structure_and_feature
-import train_structure_and_feature_2
-import train_structure_and_feature_3
-
-def main():
-    print("CHECKING...")
-
-if __name__ == "__main__":
-    main()
